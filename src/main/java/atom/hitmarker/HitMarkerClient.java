55a1c1b1d1737c6763d7b81fbc0815406dfd5e60
Mathis ALLEWEIRELDT <mathis.alleweireldt.etu@univ-lille.fr>
2026-08-02 14:36:32 +0200

    Initial commit

---
+++ src/main/java/atom/hitmarker/HitMarkerClient.java
@@ -0,0 +1,10 @@
+package atom.hitmarker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HitMarkerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
