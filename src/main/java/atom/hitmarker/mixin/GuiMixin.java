7faa3c1f147cff04c0c761f8e2f5e7964cf46507
Mathis ALLEWEIRELDT <mathis.alleweireldt.etu@univ-lille.fr>
2026-08-06 13:00:43 +0200

    Update to 1.21.2 with new render method.

--- src/main/java/atom/hitmarker/mixin/GuiMixin.java
+++ src/main/java/atom/hitmarker/mixin/GuiMixin.java
@@ -1,19 +1,24 @@
 package atom.hitmarker.mixin;
 
-import atom.hitmarker.HitMarkerClient;
-import atom.hitmarker.ModConfig;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
+import java.util.function.Function;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-    import atom.hitmarker.HitMarkerClient;
+import atom.hitmarker.HitMarkerClient;
+import atom.hitmarker.ModConfig;
+
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
@@ -28,35 +33,22 @@ public abstract class GuiMixin {
     @Redirect(
             method = "renderCrosshair",
 
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Ljava/util/function/Function;Lnet/minecraft/resources/ResourceLocation;IIII)V")
     )
-    private void redirectCrosshairBlit(GuiGraphics instance, ResourceLocation sprite, int x, int y, int width, int height) {
+    private void redirectCrosshairBlit(GuiGraphics instance,Function<ResourceLocation, RenderType> renderType, ResourceLocation sprite, int x, int y, int width, int height) {
 
-        instance.blitSprite(sprite, x, y, width, height);
+        instance.blitSprite(renderType, sprite, x, y, width, height);
 
         if (sprite.equals(CROSSHAIR_SPRITE)) {
             if (HitMarkerClient.projectileHitTimer > 0.0F) {
-
-                RenderSystem.defaultBlendFunc();
-
-                if (HitMarkerClient.isKillIndicator) {
-                    instance.setColor(1.0F, 0.0F, 0.0F, 1.0F); // Rouge
-                }
 
                 ResourceLocation spriteToUse = HIT_SPRITE_1;
                 if (ModConfig.crosshairStyle == 2) spriteToUse = HIT_SPRITE_2;
                 if (ModConfig.crosshairStyle == 3) spriteToUse = HIT_SPRITE_3;
 
-                instance.blitSprite(spriteToUse, x, y, width, height);
+                int color = HitMarkerClient.isKillIndicator ? 0xFFFF0000 : 0xFFFFFFFF;
 
-                instance.setColor(1.0F, 1.0F, 1.0F, 1.0F);
-
-                RenderSystem.blendFuncSeparate(
-                        GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR,
-                        GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR,
-                        GlStateManager.SourceFactor.ONE,
-                        GlStateManager.DestFactor.ZERO
-                );
+                instance.blitSprite(RenderType::guiTextured, spriteToUse, x, y, width, height,color);
             }
         }
     }
