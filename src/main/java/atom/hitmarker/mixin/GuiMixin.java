e77d0f3c2e5bc3619ba551e1bdead2da83ccba60
Mathis ALLEWEIRELDT <mathis.alleweireldt.etu@univ-lille.fr>
2026-08-06 13:12:40 +0200

    Update to 1.21.5 with new GlStateManager import.

--- src/main/java/atom/hitmarker/mixin/GuiMixin.java
+++ src/main/java/atom/hitmarker/mixin/GuiMixin.java
@@ -9,7 +9,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 
 
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.opengl.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import org.spongepowered.asm.mixin.Mixin;
